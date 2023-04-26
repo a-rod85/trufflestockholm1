bd5706b9e3829518dc312ac276f42f3e3ecfc184
a-rod85 <31aaronanderson@gmail.com>
2023-04-26 07:52:53 +0000

    add views.py context in contact app, add forms.py to contact app, add context to forms.py

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, 
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+from .forms import ContactForm
+
+
+def send_email(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+    else:
+        form = ContactForm()
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+
+            try:
+                send_mail(name, phone, messsage, from_email, [''])
+
+            except BadHeaderError:
+                return HttpResponse('invalid')
+
+            return HttpResponseRedirect('contact:send_success')
+
+
+    contect = {
+        'form': form,
+    }
+
+    return render(request, 'contact.html', context)
+
+
+def send_success(request):
+    return HttpResponse('We have recieved your email, Thank you')    
